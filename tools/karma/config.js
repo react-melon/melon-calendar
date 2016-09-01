c32dfb17bca115479e56568c65569480453aec75
chenxiao07 <chenxiao07@baidu.com>
2016-09-01 12:22:17 +0800

    改用 karma-webpack

--- tools/karma/config.js
+++ tools/karma/config.js
@@ -3,46 +3,41 @@
  * @author cxtom <cxtom2008@gmail.com>
  */
 
-var path = require('path');
+const path = require('path');
+const babelOptions = require('../../package.json').babel;
 
-var NODE_MODULES_FILES = '**/node_modules/**';
-
-var babelOpts = require('../../package.json').babel;
+babelOptions.plugins.push('istanbul');
 
 module.exports = {
     basePath: path.join(__dirname, '../../'),
-    frameworks: ['browserify', 'jasmine'],
+    frameworks: ['jasmine'],
     files: [
         './node_modules/jasmine-expect-jsx/dist/jasmine-expect-jsx.js', // expect-jsx
         './test/**/*.spec.js'
     ],
     browsers: ['Chrome'],
     preprocessors: {
-        './test/components/*.spec.js': ['browserify'],
-        './test/components/**/*.spec.js': ['browserify'],
-        './src/*.js': ['browserify', 'coverage']
+        './test/**/*.spec.js': ['webpack'],
+        './src/*.js': ['coverage']
     },
 
-    browserify: {
-        debug: true,
-        paths: ['./src/*.js', './test/components/**.spec.js'],
-
-        transform: [
-
-            ['babelify', babelOpts],
-
-            ['browserify-istanbul', {
-                instrumenter: require('babel-istanbul'),
-                instrumenterConfig: {
-                    babel: babelOpts
-                },
-                ignore: [
-                    NODE_MODULES_FILES
-                ]
-            }]
-        ],
-        extensions: ['.js']
+    webpack: {
+        module: {
+            preLoaders: [
+                {
+                    test: /\.js$/,
+                    loader: 'babel',
+                    exclude: /node_modules/,
+                    query: babelOptions
+                }
+            ]
+        }
     },
+
+    webpackMiddleware: {
+        stats: 'errors-only'
+    },
+
     // logLevel: config.LOG_DEBUG,
     reporters: ['coverage', 'mocha'],
     coverageReporter: {
@@ -50,7 +45,7 @@ module.exports = {
         reporters: [
             // reporters not supporting the `file` property
             {type: 'html'},
-            {type: 'lcov', subdir: 'lcov'}
+            {type: 'lcovonly', subdir: 'lcov'}
         ]
     },
     // if true, Karma captures browsers, runs the tests and exits
